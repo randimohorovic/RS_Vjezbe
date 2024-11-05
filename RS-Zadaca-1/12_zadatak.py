5c1e76f6bb51673f247d6524fe870c96b0f1b026
Randi Mohorović <randi.mohorovic@gmail.com>
2024-11-05 20:12:38 +0100

    12 zadatak

---
+++ RS-Zadaca-1/12_zadatak.py
@@ -0,0 +1,12 @@
+def obrni_rjecnik(rjecnik):
+    
+    obrnuti_rjecnik={}
+    for key, value in rjecnik.items():
+         obrnuti_rjecnik.update({value: key})
+    return obrnuti_rjecnik
+
+rjecnik = {"ime": "Ivan", "prezime": "Ivić", "dob": 25}
+print(obrni_rjecnik(rjecnik))
+
+
+# {'Ivan': 'ime', 'Ivić': 'prezime', 25: 'dob'}
